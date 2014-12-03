30b6fd3270b12dea505195cf921495144d2231a0
Sam Krishna <sam.krishna@gmail.com>
2014-12-02 18:46:59 -0800

    Removed extraneous 'Task' prefix from header #ifndef.

--- Task/TaskMacros.h
+++ Task/TaskMacros.h
@@ -24,15 +24,15 @@
 //  THE SOFTWARE.
 //
 
-#ifndef Task_TaskMacros_h
-#define Task_TaskMacros_h
+#ifndef TaskMacros_h
+#define TaskMacros_h
 
 #ifndef NS_DESIGNATED_INITIALIZER
-#if __has_attribute(objc_designated_initializer)
-#define NS_DESIGNATED_INITIALIZER __attribute__((objc_designated_initializer))
-#else
-#define NS_DESIGNATED_INITIALIZER
-#endif
+    #if __has_attribute(objc_designated_initializer)
+        #define NS_DESIGNATED_INITIALIZER __attribute__((objc_designated_initializer))
+    #else
+        #define NS_DESIGNATED_INITIALIZER
+    #endif
 #endif
 
 #endif
